14237f678c7e72e95bd383a49939642ad4c6d365
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-12-17 18:40:14 +0300

    feat: create company statement component

---
+++ src/components/splash/companystatement.tsx
@@ -0,0 +1,13 @@
+
+export default function CompanyStatement() {
+  return (
+    <div className="grid grid-rows-2 gap-2 w-full">
+    <div className="grid justify-items-center text-purple-950 ">
+      <h2>from</h2>
+    </div>
+    <div className="grid justify-items-center text-purple-950 font-bold text-xl">
+      <h2>GigaDesk</h2>
+    </div>
+    </div>
+  );
+}
