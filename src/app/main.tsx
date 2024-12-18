fc42b7ed987bd33a78f1698eee6c8d568db0ccff
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-12-18 17:43:09 +0300

    feat: add home page

---
+++ src/app/main.tsx
@@ -0,0 +1,11 @@
+import NextButton from "./buttons/nextbutton";
+import StudentSchoolSurvey from "@/components/question/studentschoolsurvey";
+
+export default function Main() {
+  return (
+    <div className="grid content-center justify-items-center grid-rows-2 gap-8">
+      <StudentSchoolSurvey />
+      <NextButton />
+    </div>
+  );
+}
