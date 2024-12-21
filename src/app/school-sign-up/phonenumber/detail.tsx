25a7fa3641d28e3f12149b1bad893ffa1898e12d
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-12-21 08:29:41 +0300

    feat: execute a check school phone number existence query on the server

--- src/app/school-sign-up/phonenumber/detail.tsx
+++ src/app/school-sign-up/phonenumber/detail.tsx
@@ -1,12 +1,26 @@
 "use client";
 
-import InputPhoneNumber from "@/components/Input/inputphonenumber";
 import { SchoolSignupInstance } from "@/state/store";
 import { useEffect } from "react";
 import { useSnapshot } from "valtio";
 import { SchoolSignup } from "@/state/store";
+import { useLazyQuery, gql } from "@apollo/client";
+import Button from "@mui/joy/Button";
+import Stack from "@mui/joy/Stack";
+import MuiPhoneNumber from "mui-phone-number";
+
+const CHECK_SCHOOL_PHONENUMBER_EXISTENCE_QUERY = gql`
+  query checkphonenumber($phone_number: String!) {
+    schoolPhoneNumberExists(phone_number: $phone_number) {
+      verified
+      unverified
+    }
+  }
+`;
 
 export default function Detail() {
+  let phoneNumber: string;
+
   const snap = useSnapshot(SchoolSignupInstance);
 
   const handleChange = (value: any) => {
@@ -23,12 +37,45 @@ export default function Detail() {
 
   useEffect(() => {
     window.localStorage.setItem("SchoolSignUp", JSON.stringify(snap.instance));
+    phoneNumber = snap.instance.phoneNumber;
+    console.log(phoneNumber);
   }, [snap.instance]);
 
+  const [checkPhoneNumber, { loading, error, data }] = useLazyQuery(
+    CHECK_SCHOOL_PHONENUMBER_EXISTENCE_QUERY
+  );
+
   return (
-    <InputPhoneNumber
-      handlechange={handleChange}
-      disableSubmit={SchoolSignupInstance.instance.phoneNumber.length != 13}
-    />
+    <div className="max-md:px-2">
+      <Stack spacing={1}>
+        <MuiPhoneNumber
+          defaultCountry={"ke"}
+          onChange={handleChange}
+          onlyCountries={["ke"]}
+          countryCodeEditable={false}
+          value={snap.instance.phoneNumber}
+        />
+        <div className="text-red-600">{error?.message}</div>
+        <Button
+          type="submit"
+          color="primary"
+          sx={{
+            "&.MuiButton-colorPrimary": {
+              backgroundColor: "#3B0764",
+              "&:hover": {
+                backgroundColor: "#581C87",
+              },
+            },
+          }}
+          disabled={snap.instance.phoneNumber.length != 13}
+          onClick={() =>
+            checkPhoneNumber({ variables: { phone_number: phoneNumber } })
+          }
+          loading={loading}
+        >
+          Submit
+        </Button>
+      </Stack>
+    </div>
   );
 }
