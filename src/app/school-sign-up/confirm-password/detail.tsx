c8a46da99eb0bc0334108b85783e7c652f8f70ad
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-12-22 08:40:45 +0300

    feat: send sign up data

--- src/app/school-sign-up/confirm-password/detail.tsx
+++ src/app/school-sign-up/confirm-password/detail.tsx
@@ -5,6 +5,24 @@ import { SchoolSignup, SchoolSignupInstance } from "@/state/store";
 import Button from "@mui/joy/Button";
 import { useEffect, useState } from "react";
 import { useSnapshot } from "valtio";
+import { useMutation } from "@apollo/client";
+import { gql } from "../../../__generated__/gql";
+
+const CREATE_SCHOOL_MUTATION = gql(`
+  mutation createSchool($newschool: NewSchool!) {   
+    createSchool( input: $newschool ) {    
+      id
+      createdAt
+      updatedAt
+      deletedAt
+      name
+      phone_number
+      password
+      badge
+      Website
+     }    
+ }
+`);
 
 export default function Detail() {
   const snap = useSnapshot(SchoolSignupInstance);
@@ -27,12 +45,18 @@ export default function Detail() {
     window.localStorage.setItem("SchoolSignUp", JSON.stringify(snap.instance));
   }, [snap.instance]);
 
+  // our mutation's result, data, is typed!
+  const [createSchool, { data, loading, error }] = useMutation(
+    CREATE_SCHOOL_MUTATION
+  );
+
   return (
     <div className="grid">
       <InputPasswordConfirmation
         handlechange={handleChange}
         expectedValue={snap.instance.password}
       />
+      <div className="text-red-600 mb-4 text-center">{error?.message}</div>
       <Button
         type="submit"
         color="primary"
@@ -45,8 +69,25 @@ export default function Detail() {
           },
         }}
         disabled={snap.instance.password != value}
+        loading={loading}
+        onClick={(e) => {
+          e.preventDefault();
+          createSchool({
+            variables: {
+              newschool: {
+                name: snap.instance.name,
+                phone_number: snap.instance.phoneNumber,
+                password: snap.instance.password,
+              },
+            },
+          }).catch((res) => {
+            const errors = res.graphQLErrors.map((error: any) => {
+              console.log(error.message);
+            });
+          });
+        }}
       >
-        Submit
+        Sign up
       </Button>
     </div>
   );
