11f05f1c879d5076151f6b958162d1e3704076e0
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-12-23 14:32:15 +0300

    feat: send request school password reset verification data

--- src/app/school-sign-in/request-password-reset/detail.tsx
+++ src/app/school-sign-in/request-password-reset/detail.tsx
@@ -1,17 +1,47 @@
 "use client";
 
 import Button from "@mui/joy/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Stack from "@mui/joy/Stack";
 import Input from "@mui/joy/Input";
 import SendOtp from "@/app/school-sign-up/verify-phonenumber/sendotp";
+import { gql } from "@/__generated__/gql";
+import { useMutation } from "@apollo/client";
+
+const REQUEST_SCHOOL_PASSWORD_RESET_MUTATION = gql(`
+   mutation requestSchoolPasswordReset($verificationinfo: verificationinfo!){
+      requestSchoolPasswordReset(input: $verificationinfo)
+   }
+`);
 
 export default function Detail() {
   const [otp, setOtp] = useState("");
+  const [phonenumber, setPhoneNumber] = useState("");
 
   const handleChange = (event: any) => {
     setOtp(event.target.value);
   };
+
+  useEffect(() => {
+    const data = window.localStorage.getItem("ForgotPasswordPhoneNumber");
+    if (data !== null) {
+      const Parseddata: string = JSON.parse(data);
+      setPhoneNumber(Parseddata);
+    }
+  }, []);
+
+  const [requestSchoolPasswordReset, { data, loading, error }] = useMutation(
+    REQUEST_SCHOOL_PASSWORD_RESET_MUTATION
+  );
+
+  useEffect(() => {
+    if (data !== undefined && data !== null) {
+      window.localStorage.setItem(
+        "AuthenticationToken",
+        JSON.stringify(data.requestSchoolPasswordReset)
+      );
+    }
+  }, [data]);
 
   return (
     <div className="grid">
@@ -28,6 +58,7 @@ export default function Detail() {
             },
           }}
         />
+        <div className="text-red-600 mb-4 text-center">{error?.message}</div>
         <Button
           type="submit"
           color="primary"
@@ -40,10 +71,26 @@ export default function Detail() {
             },
           }}
           disabled={otp.length != 6}
+          loading={loading}
+          onClick={(e) => {
+            e.preventDefault();
+            requestSchoolPasswordReset({
+              variables: {
+                verificationinfo: {
+                  phone_number: phonenumber,
+                  otp: otp,
+                },
+              },
+            }).catch((res) => {
+              const errors = res.graphQLErrors.map((error: any) => {
+                console.log(error.message);
+              });
+            });
+          }}
         >
           Verify
         </Button>
-        <SendOtp phonenumber="" />
+        <SendOtp phonenumber={phonenumber} />
       </Stack>
     </div>
   );
