b2e0199ba1ec33c9c25229a669c863ec32e04526
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-18 19:56:13 +0300

    feat: add navbar

---
+++ src/app/school/navbar.tsx
@@ -0,0 +1,22 @@
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+export default function Navbar() {
+  return (
+    <div className=" flex justify-between">
+      <div className="grid content-center p-2">
+        <button
+          className="p-2 rounded-lg text-black text-lg hover:bg-hover-gray"
+          style={{ fontFamily: "McLaren" }}
+        >
+          eardrum
+          <ArrowDropDownIcon />
+        </button>
+      </div>
+      <div className="grid content-center p-4">
+        <div className="rounded-full hover:bg-hover-gray w-[48px] h-[48px] p-[4px] transition-all duration-700 delay-300 ease-in-out">
+          <button className="rounded-full w-[40px] h-[40px] bg-white border border-border-gray"></button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -1,10 +1,14 @@
 import Sidebar from "./sidebar";
+import Navbar from "./navbar";
 
 export default function School() {
   return (
     <div className="min-h-screen bg-white flex flex-row">
       <Sidebar />
-      <div></div>
+      <div className="grow grid grid-rows-[74px_1fr]">
+        <Navbar />
+        <div className=""></div>
+      </div>
     </div>
   );
 }
