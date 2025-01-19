fa0401cb7694599079131a3b2c62c02c1f7329a3
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-19 09:49:10 +0300

    fix: apply McLaren font globally in the school page

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -3,7 +3,10 @@ import Navbar from "./navbar";
 
 export default function School() {
   return (
-    <div className="min-h-screen bg-white flex flex-row">
+    <div
+      className="min-h-screen bg-white flex flex-row"
+      style={{ fontFamily: "McLaren" }}
+    >
       <Sidebar />
       <div className="grow grid grid-rows-[74px_1fr]">
         <Navbar />
