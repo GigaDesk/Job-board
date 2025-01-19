3f1353d83db8ad1112d908334c1aac6f67bc9d46
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-19 12:06:14 +0300

    feat: add school profile popup

--- src/app/school/navbar.tsx
+++ src/app/school/navbar.tsx
@@ -1,14 +1,11 @@
 import EardrumStatement from "./eardrum-statement";
+import SchoolProfile from "./school-profile";
 
 export default function Navbar() {
   return (
     <div className=" flex justify-between">
       <EardrumStatement />
-      <div className="grid content-center p-4">
-        <div className="rounded-full hover:bg-hover-gray w-[48px] h-[48px] p-[4px] transition-all duration-700 delay-300 ease-in-out">
-          <button className="rounded-full w-[40px] h-[40px] bg-white border border-border-gray"></button>
-        </div>
-      </div>
+      <SchoolProfile />
     </div>
   );
 }
