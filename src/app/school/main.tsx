d192784f8cac1adbb9582a184ca0e4d85797a1d1
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-21 12:28:55 +0300

    feat: add routes toggle and popup routes

--- src/app/school/main.tsx
+++ src/app/school/main.tsx
@@ -1,11 +1,13 @@
-import EardrumStatement from "./eardrum-statement";
+import PopupButton from "./popup-button";
 import CompanyStatement from "@/components/splash/companystatement";
+import RoutesToggle from "./routes-toggle";
+import PopupRoutes from "./popup-routes";
 
 export default function Main() {
   return (
     <div className=" grid grid-rows-[60px_390px_80px]">
       <div className=" grid justify-items-start">
-        <EardrumStatement />
+        <PopupButton button={<RoutesToggle />} popupElement={<PopupRoutes />} />
       </div>
       <div className="overflow-auto grid justify-items-center [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
         <div className=" w-[700px] text-black"></div>
