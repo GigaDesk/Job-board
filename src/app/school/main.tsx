57fcec3332de41c110e48ddc3ffd3237811cf15c
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-21 17:51:53 +0300

    feat: add student table component

--- src/app/school/main.tsx
+++ src/app/school/main.tsx
@@ -2,6 +2,7 @@ import PopupButton from "./popup-button";
 import CompanyStatement from "@/components/splash/companystatement";
 import RoutesToggle from "./routes-toggle";
 import PopupRoutes from "./popup-routes";
+import StudentTable from "./student-table";
 
 export default function Main() {
   return (
@@ -10,7 +11,9 @@ export default function Main() {
         <PopupButton button={<RoutesToggle />} popupElement={<PopupRoutes />} />
       </div>
       <div className="overflow-auto grid justify-items-center [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
-        <div className=" w-[700px] text-black"></div>
+        <div className="w-[700px] text-black">
+          <StudentTable />
+        </div>
       </div>
       <div className="grid content-center">
         <CompanyStatement />
