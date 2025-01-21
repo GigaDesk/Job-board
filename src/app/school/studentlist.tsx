455da0f76c861068bfd013e503df81fcfb26924a
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-21 17:52:17 +0300

    feat: add student list component

---
+++ src/app/school/studentlist.tsx
@@ -0,0 +1,123 @@
+export default function StudentList() {
+  return (
+    <div className="rounded-b-xl text-black overflow-auto h-[270px] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+      <div className="px-2">
+        <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+          <div className="grid grid-cols-3">
+            <div className="">Leon Kenyaga</div>
+            <div className=""> SCT-251-035/2019</div>
+            <div className="">11/12/2002</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
