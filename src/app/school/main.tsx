8279050fbb3beb97e5faeaa9e871efa19cabc15f
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-23 18:35:34 +0300

    fix: adjust main component padding for mobile

--- src/app/school/main.tsx
+++ src/app/school/main.tsx
@@ -14,8 +14,8 @@ export default function Main() {
           popupPlacement="bottom-start"
         />
       </div>
-      <div className="overflow-auto grid justify-items-center [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
-        <div className="w-[700px] text-black">
+      <div className="overflow-auto grid justify-items-center [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full max-md:p-2">
+        <div className="md:w-[700px] text-black">
           <StudentTable />
         </div>
       </div>
