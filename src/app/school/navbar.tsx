97d7834fd0004ba01766450ebbe89f5212ba13e5
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-23 16:10:52 +0300

    fix: add menu icon on mobile sidebar

--- src/app/school/navbar.tsx
+++ src/app/school/navbar.tsx
@@ -2,15 +2,21 @@ import PopupButton from "./popup-button";
 import SchoolProfile from "./school-profile";
 import EardrumStatement from "./eardrum-statement";
 import ProductContent from "./product-content";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Navbar() {
   return (
     <div className=" flex justify-between">
+      <div className="max-lg:grid max-lg:grid-cols-[50px_1fr]">
+      <div className="lg:hidden p-4 grid content-center">
+      <MenuIcon sx={{ color: "#1C2B33" }} />
+      </div>
       <PopupButton
         button={<EardrumStatement />}
         popupElement={<ProductContent />}
         popupPlacement="bottom-start"
       />
+      </div>
       <SchoolProfile />
     </div>
   );
