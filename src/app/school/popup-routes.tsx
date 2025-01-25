cd0d448847ab530f2897ae2a16909bbe4c404c50
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-01-25 18:49:32 +0300

    fix: fix popup routes component

--- src/app/school/popup-routes.tsx
+++ src/app/school/popup-routes.tsx
@@ -13,7 +13,7 @@ export default function PopupRoutes() {
           style={{ fontFamily: "McLaren" }}
         >
           <Stack spacing={1}>
-            <div className="hover:bg-hover-gray cursor-pointer px-4 p-2">
+            <div className="hover:bg-hover-gray hover:text-black cursor-pointer px-4 p-2 bg-light-blue text-bright-blue">
               <h4 className="font-medium">Home</h4>
             </div>
           </Stack>
