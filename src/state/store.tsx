d3692c8273ddac79b7607943f54728c2f3dc148a
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-02-07 00:45:37 +0300

    refactor: relocate states to their related route group folders

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -4,48 +4,12 @@ export interface StudentSchoolSurveyAnswer {
   answer: "school" | "student" | "";
 }
 
-export interface SchoolSignup {
-  name: string;
-  phoneNumber: string;
-  password: string;
-}
-
-
-
-export interface ForgotStudentPassword {
-  schoolid: number;
-  registrationNumber: string;
-  phoneNumber: string;
-  otp: string;
-  newpassword: string;
-}
-
 export const StudentSchoolSurveyInstance = proxy({
   instance: { answer: "" } as StudentSchoolSurveyAnswer,
 });
 
-export const SchoolSignupInstance = proxy({
-  instance: { name: "", phoneNumber: "", password: "" } as SchoolSignup,
-});
-
-
-
-export const ForgotStudentPasswordInstance = proxy({
-  instance: {
-    schoolid: 0,
-    registrationNumber: "",
-    phoneNumber: "",
-    otp: "",
-    newpassword: "",
-  } as ForgotStudentPassword,
-});
-
 export const AuthenticationToken = proxy({
   token: "" as string | null | undefined,
-});
-
-export const SchoolPasswordResetValue = proxy({
-  password: "" as string,
 });
 
 export const SidebarState = proxy({
