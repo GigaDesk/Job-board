1f986c55af82586b28ee5ff15417f3fd66c45826
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-02-23 19:07:41 +0300

    feat: add Unapproved Jobs navigation option

--- src/components/drawer/pages-section.tsx
+++ src/components/drawer/pages-section.tsx
@@ -1,15 +1,23 @@
 import { Stack } from "@mui/joy";
+import Link from "next/link";
 
 //PagesSection shows the navigable pages of the application
 export default function PagesSection() {
   return (
-      <div className="p-4 text-black">
-        <Stack spacing={2}>
-          <div className="">Pages</div>
+    <div className="p-4 text-black">
+      <Stack spacing={2}>
+        <div className="">Pages</div>
+        <Link href="/admin">
           <div className="pl-4 py-2 rounded-full bg-light-blue text-bright-blue hover:bg-hover-gray hover:text-black cursor-pointer">
             Home
           </div>
-        </Stack>
-      </div>
+        </Link>
+        <Link href="/admin/unapproved-jobs">
+          <div className="pl-4 py-2 rounded-full bg-light-blue text-bright-blue hover:bg-hover-gray hover:text-black cursor-pointer">
+            Unapproved Jobs
+          </div>
+        </Link>
+      </Stack>
+    </div>
   );
 }
