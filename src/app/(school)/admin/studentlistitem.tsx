85a560a9b8907ee8080bcaa9e8292f0e2d9d6ba0
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-03-04 13:21:43 +0300

    feat: fetch unapproved jobs data

--- src/app/(school)/admin/studentlistitem.tsx
+++ src/app/(school)/admin/studentlistitem.tsx
@@ -1,6 +1,6 @@
 export interface StudentListItemProps {
   name?: string;
-  registration_number?: string;
+  registration_number?: string | null;
   phone_number?: string;
 }
 
@@ -8,12 +8,15 @@ export default function StudentListItem(props: StudentListItemProps) {
   return (
     <div className="px-2">
       <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-3">
           <div className="h-[20px] overflow-x-clip overflow-y-clip">
             {props.name}
           </div>
           <div className="h-[20px] overflow-x-clip overflow-y-clip">
             {props.registration_number}
+          </div>
+          <div className="h-[20px] overflow-x-clip overflow-y-clip">
+            {props.phone_number}
           </div>
         </div>
       </div>
