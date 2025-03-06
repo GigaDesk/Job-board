92419bcc81f1452ca39f03de01c5888c67d3e47f
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-03-07 02:33:44 +0300

    feat: implement unapproved job approval funstionality

--- src/app/(admin)/admin/studentlistitem.tsx
+++ src/app/(admin)/admin/studentlistitem.tsx
@@ -7,15 +7,15 @@ export interface StudentListItemProps {
 export default function StudentListItem(props: StudentListItemProps) {
   return (
     <div className="px-2">
-      <div className="px-2 border-b border-border-table-gray h-[30px] grid items-center">
+      <div className="px-2 border-b border-border-table-gray h-[55px] grid items-center">
         <div className="grid grid-cols-3">
-          <div className="h-[20px] overflow-x-clip overflow-y-clip">
+          <div className="h-[55px] overflow-x-clip overflow-y-clip">
             {props.name}
           </div>
-          <div className="h-[20px] overflow-x-clip overflow-y-clip">
+          <div className="h-[55px] overflow-x-clip overflow-y-clip">
             {props.registration_number}
           </div>
-          <div className="h-[20px] overflow-x-clip overflow-y-clip">
+          <div className="h-[55px] overflow-x-clip overflow-y-clip">
             {props.phone_number}
           </div>
         </div>
