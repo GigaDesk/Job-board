2095c5112597b30210127ab772fae1a769f8b6a2
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-03-08 18:19:21 +0300

    feat: add route update functionality

--- src/app/(admin)/admin/@home/unapproved-jobs/page.tsx
+++ src/app/(admin)/admin/@home/unapproved-jobs/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
 import UnapprovedJobsTable from "./unapproved-jobs-table";
+import { ActiveRoute } from "@/app/(admin)/state/store";
 
 export default function Home() {
+
+  useEffect(() => {
+    ActiveRoute.instance = "Unapproved Jobs"
+  }, []);
+
   return (
     <div className="overflow-auto grid justify-items-center [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full max-md:p-2">
       <div className="md:w-[700px] lg:w-[900px] text-black">
