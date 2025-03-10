c11f8ee7e41b7eb080845025079cc71322c48da7
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-03-10 09:55:56 +0300

    fix: reroute to new job page on add button click in jobs table

--- src/app/(admin)/admin/student-table.tsx
+++ src/app/(admin)/admin/student-table.tsx
@@ -32,11 +32,13 @@ export default function StudentTable() {
     <div className="h-[350px] rounded-xl grid grid-rows-[40px_1fr] border border-border-table-gray">
       <div className="rounded-t-xl border-b border-border-table-gray px-2 md:px-4 flex justify-between items-center text-text-table-gray">
         Jobs
-        <PopupButton
-          button={<AddStudent />}
-          popupElement={<AddStudentPopup />}
-          popupPlacement="bottom-end"
-        />
+        <button
+          onClick={() => {
+            router.push(`/admin/new-job`);
+          }}
+        >
+          <AddStudent />
+        </button>
       </div>
       <div className="rounded-b-xl grid  grid-rows-[50px_1fr] md:grid-rows-[30px_1fr]">
         <div className="border-b border-border-table-gray grid grid-cols-3 px-4 md:px-4 md:pr-6 items-center text-text-table-gray">
