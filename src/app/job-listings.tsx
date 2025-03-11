6901f3134f50a0abec2804081a43918da9890582
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-03-11 09:30:52 +0300

    feat: add cvposter component

--- src/app/job-listings.tsx
+++ src/app/job-listings.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from "@apollo/client";
 import Job from "./job";
 import { gql } from "@/__generated__";
+import CvPoster from "./cvposter";
 
 const GET_JOB_LISTING_QUERY = gql(`
 query getlistingJobs {
@@ -18,20 +19,25 @@ query getlistingJobs {
 export default function JobListings() {
   const { data } = useQuery(GET_JOB_LISTING_QUERY);
   return (
-    <div className="w-full p-4 overflow-auto grid md:grid-cols-2 lg:grid-cols-3 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
-      {data?.getJobs?.map((job) => (
-        <div
-          key={job.id}
-          className="h-96 grid content-center justify-items-center"
-        >
-          <Job
-            id={job.id}
-            title={job.title}
-            location={job.location}
-            description={job.description}
-          />
-        </div>
-      ))}
+    <div className="w-full p-4 overflow-auto  [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
+     <div className="px-8 md:px-8 lg:px-16">
+     <CvPoster/>
+     </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3">
+        {data?.getJobs?.map((job) => (
+          <div
+            key={job.id}
+            className="h-96 grid content-center justify-items-center"
+          >
+            <Job
+              id={job.id}
+              title={job.title}
+              location={job.location}
+              description={job.description}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
