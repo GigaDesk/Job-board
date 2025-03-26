cd16d7cbd66f665623dfe2111e64e158522779ab
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-03-26 10:19:32 +0300

    fix: limit height of title content in job card to prevent card over flow

--- src/app/job.tsx
+++ src/app/job.tsx
@@ -38,9 +38,11 @@ export default function Job(props: JobProps) {
         <div className=" grid content-center pl-4">{props.company}</div>
       </div>
       <CardContent>
-        <Typography level="title-lg">{props.title}</Typography>
+        <div className="h-12 overflow-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
+          <Typography level="title-lg">{props.title}</Typography>
+        </div>
         <Typography level="body-md"> {props.location} </Typography>
-        <div className="mt-4 h-40 overflow-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
+        <div className="mt-4 h-32 overflow-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full">
           <Typography level="body-sm">{props.description}</Typography>
         </div>
       </CardContent>
