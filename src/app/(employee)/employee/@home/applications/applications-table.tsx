0b480a9713cc3d7a974cbee0486660a45ece419a
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-03-28 14:37:48 +0300

    fix: correct build errors

--- src/app/(employee)/employee/@home/applications/applications-table.tsx
+++ src/app/(employee)/employee/@home/applications/applications-table.tsx
@@ -1,35 +1,11 @@
 "use client";
 
 import StudentList from "../../studentlist";
-import { gql } from "../../../../../__generated__/gql";
-import { useQuery } from "@apollo/client";
 import StudentListItem from "../../studentlistitem";
-import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-
-const GET_EMPLOYER_UNAPPROVED_JOBS_QUERY = gql(`
-query getEmployerProfile2{
-  getEmployerProfile{
-    unapprovedJobs{
-      id
-      title
-      level
-      deadline
-    }
-  }
-}
-`);
 
 export default function ApplicationsTable() {
   const router = useRouter();
-
-  const { data, refetch } = useQuery(GET_EMPLOYER_UNAPPROVED_JOBS_QUERY, {
-    fetchPolicy: "network-only",
-  });
-
-  useEffect(() => {
-    refetch();
-  }, [refetch]);
 
   return (
     <div className="h-[350px] rounded-xl grid grid-rows-[40px_1fr] border border-border-table-gray">
@@ -43,20 +19,20 @@ export default function ApplicationsTable() {
           <div className="max-md:hidden">Status </div>
         </div>
         <StudentList>
-            <div className="grid">
-              <div
-                className="cursor-pointer"
-                onClick={() => {
-                  router.push(`/employee/applications`);
-                }}
-              >
-                <StudentListItem
-                  name={"Machine Learning Engineer"}
-                  registration_number={"Safaricom"}
-                  phone_number={"Pending"}
-                />
-              </div>
+          <div className="grid">
+            <div
+              className="cursor-pointer"
+              onClick={() => {
+                router.push(`/employee/applications`);
+              }}
+            >
+              <StudentListItem
+                name={"Machine Learning Engineer"}
+                registration_number={"Safaricom"}
+                phone_number={"Pending"}
+              />
             </div>
+          </div>
         </StudentList>
       </div>
     </div>
