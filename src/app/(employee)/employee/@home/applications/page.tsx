5d06b555b43877f3d6c592a48b44a123f7b83963
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-04-02 15:36:58 +0300

    feat: implement and fetch ApplicationsTable, AccepetedApplicationsTable, RejectedApplicationsTable and PendingApplicationsTable

--- src/app/(employee)/employee/@home/applications/page.tsx
+++ src/app/(employee)/employee/@home/applications/page.tsx
@@ -2,18 +2,26 @@
 
 import { useEffect } from "react";
 import ApplicationsTable from "./applications-table";
+import AcceptedApplicationsTable from "./accepted-applications-table";
+import RejectedApplicationsTable from "./rejected-applications-table";
+import PendingApplicationsTable from "./pending-applications-table";
 import { ActiveRoute } from "../../../state/store";
+import Stack from "@mui/joy/Stack";
 
 export default function Home() {
-
   useEffect(() => {
-    ActiveRoute.instance = "Applications"
+    ActiveRoute.instance = "Applications";
   }, []);
 
   return (
     <div className="overflow-auto grid justify-items-center [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-border-gray hover:[&::-webkit-scrollbar-thumb]:bg-hover-gray  [&::-webkit-scrollbar-thumb]:rounded-full max-md:p-2">
       <div className="w-full md:w-[700px] lg:w-[900px] text-black">
+      <Stack spacing={5}>
         <ApplicationsTable />
+        <AcceptedApplicationsTable />
+        <RejectedApplicationsTable />
+        <PendingApplicationsTable />
+      </Stack>
       </div>
     </div>
   );
